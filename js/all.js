b55633b6aabd801513b1726c0dece71e6a024ba2
bensoen <marilyn780913@gmail.com>
2021-01-27 10:33:03 +0800

    註解一

---
+++ js/all.js
@@ -0,0 +1,32 @@
+// var farms =[
+//     {
+//         farmer:'卡斯伯'
+//     },
+//     {
+//         farmer: '洧杰'
+//     }
+// ]
+// console.log(farms[1].farmer);
+
+
+
+var farms = [
+    {
+        farmer: '卡斯伯',
+        dogs: ['張姆士', '龐的'],
+        chick: 15,
+        cornField: [8, 5, 6],
+        broccoliField: [6, 6, 6, 6],
+        scarecrow: 9
+    },
+    {
+        farmer: '查理',
+        dogs: ['皮皮'],
+        chick: 30,
+        cornField: [18, 12],
+        broccoliField: [8, 8, 8],
+        scarecrow: 6
+    }
+];
+
+console.log(farms[1].dogs[0])
